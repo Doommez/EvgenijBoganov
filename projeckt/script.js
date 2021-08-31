2560f8414e7985f16fc9a431d8d7a7da68c88d16
doommez <jenyaz862@gmail.com>
2021-08-31 23:41:08 +0300

    87

--- projeckt/script.js
+++ projeckt/script.js
@@ -1,6 +1,8 @@
 var boxMove=document.getElementById("box__move"),
 bullet=document.getElementById("bullet"),
 container=document.getElementById("container");
+var cBulletX=bullet.offsetLeft-bullet.offsetWidth/2;
+var cBulletY=bullet.offsetTop-bullet.offsetHeight/2;
 console.log(container);
 document.addEventListener("mousemove",boxRot);
 function boxRot(ev){
@@ -8,7 +10,12 @@ function boxRot(ev){
     var cx = bb.left + bb.width / 2, cy = bb.top + bb.height / 2;
     var angle = Math.atan2(ev.y - cy, ev.x - cx);
     
-    boxMove.style.transform = "rotate(" + angle + "rad)"
+    boxMove.style.transform = "rotate(" + angle + "rad)";
+    document.addEventListener("click",shut);
+    function shut(ev){
+        bullet.style.top=ev.y+bullet.offsetHeight+20+"px";
+        bullet.style.left=ev.x-bullet.offsetWidth/2+"px"
+        }
 }
 let areaH=[
     [0,0]
@@ -27,6 +34,7 @@ for(let i=0;i<areaH.length;i++){
 function creatPole0(){
     let div=document.createElement("div");
     div.className="pole0";
-    container.appendChild(div)
+    document.body.appendChild(div)
 
 }
+
