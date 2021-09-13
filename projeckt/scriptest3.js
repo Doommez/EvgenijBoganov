f61b31bf02ab4af51e4a4ae9e269e3384cd58ee4
doommez <jenyaz862@gmail.com>
2021-09-13 16:51:11 +0300

    qwer

--- projeckt/scriptest3.js
+++ projeckt/scriptest3.js
@@ -1,11 +1,11 @@
 canvas=document.getElementById("canvas");
 ctx=canvas.getContext('2d');
 
-
+var cluster=[];
 var bubble=new Image();
 bubble.onload=drow;
 bubble.src="bubble.png"
-    
+var gridpos;
 //qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
 
   var arcX  = 320;
@@ -154,26 +154,33 @@ function pullbubble(){
                 if(collis(b.pos[0],b.pos[1],20,level.tiles[w][q].cord.tilex,level.tiles[w][q].cord.tiley)){
                     b.to[0]=0;
                     b.to[1]=0; 
-                   /*  if(b.pos[0]-level.tiles[w][q].cord.tilex<=-20){
+                    if(b.pos[0]-level.tiles[w][q].cord.tilex<0){
                          b.pos[1]=level.tiles[w][q].cord.tiley+35;
                     b.pos[0]=level.tiles[w][q].cord.tilex-tileheight/2;
-                    }else if(b.pos[0]-level.tiles[w][q].cord.tilex>=20){
+                    }else if(b.pos[0]-level.tiles[w][q].cord.tilex>0){
                         b.pos[1]=level.tiles[w][q].cord.tiley+35;
-                        b.pos[0]=level.tiles[w][q].cord.tilex+tileheight;
+                        b.pos[0]=level.tiles[w][q].cord.tilex+tileheight/2;
                     }
-                    */
+                    
                     var centerX=b.pos[0];
                    var centerY= b.pos[1];
-                   var gridpos = getGridPosition(centerX, centerY);
+                    gridpos = getGridPosition(centerX, centerY);
                         
-                   level.tiles[w+1].push({cord:{tilex:b.pos[0],tiley:b.pos[1]},type:0})
+                   level.tiles[gridpos.y][gridpos.x]={cord:{tilex:b.pos[0],tiley:b.pos[1]},type:0}
                         bullets=[]
+                        cluster = findCluster(gridpos.x,gridpos.y,  level.tiles[gridpos.y][gridpos.x].type, true, false);
+                                   
+                        if (cluster.length >= 3) {
+                            // Remove the cluster
+                            setGameState(gamestates.removecluster);
+                            return;
+                        } 
                 }
-             
+           
             }  
         }
        }
-        
+     
         if(b.pos[0]<1 ||b.pos[0]>canvas.width-1 ){
  b.to[0]=-b.to[0];
                     } 
@@ -183,7 +190,7 @@ function pullbubble(){
             b.to[0]=0;
             b.to[1]=0;
         }
-    
+        endGame();
         // рисуем его
         ctx.beginPath();
         ctx.arc(...b.pos, 5, 0, 2 * Math.PI);
@@ -209,12 +216,108 @@ function getGridPosition(x, y) {
  
     // Check for offset
     var xoffset = 0;
-    if ((gridy + rowheight) % 2) {
+    if ((gridy) % 2) {
         xoffset = tilewidth / 2;
     }
     var gridx = Math.floor((x - xoffset) / tilewidth);
  
     return { x: gridx, y: gridy };
 }
+
 console.log(level);
 
+function endGame(){
+    for(let n=0;n<level.columns;n++){
+        if(level.tiles[level.rows-1].length==0){
+            return false
+        }else if(level.tiles[level.rows-1].length>0) {
+            alert("asdf");
+            render();
+            return true
+    }
+    
+}}
+function findCluster(ty, tx, matchtype, reset, skipremoved) {
+    // Reset the processed flags
+ 
+    
+    // Get the target tile. Tile coord must be valid.
+    var targettile = level.tiles[tx][ty];
+    
+    // Initialize the toprocess array with the specified tile
+    var toprocess = [targettile];
+   
+    var foundcluster = [];
+
+    while (toprocess.length > 0) {
+        // Pop the last element from the array
+        var currenttile = toprocess.pop();
+        
+        // Skip processed and empty tiles
+        if (currenttile.type == -1) {
+            continue;
+        }
+        
+        // Skip tiles with the removed flag
+        if (skipremoved && currenttile.removed) {
+            continue;
+        }
+        
+        // Check if current tile has the right type, if matchtype is true
+        if (!matchtype || (currenttile.type == targettile.type)) {
+            // Add current tile to the cluster
+            foundcluster.push(currenttile);
+            
+            // Get the neighbors of the current tile
+            var neighbors = getNeighbors(currenttile);
+            
+            // Check the type of each neighbor
+            for (var i=0; i<neighbors.length; i++) {
+                if (!neighbors[i].processed) {
+                    // Add the neighbor to the toprocess array
+                    toprocess.push(neighbors[i]);
+                    neighbors[i].processed = true;
+                }
+            }
+        }
+    }
+    
+    // Return the found cluster
+    return foundcluster;
+}
+function resetProcessed() {
+    for (var i=0; i<level.columns; i++) {
+        for (var j=0; j<level.rows; j++) {
+            level.tiles[i][j].processed = false;
+        }
+    }
+}
+ 
+// Neighbor offset table
+var neighborsoffsets = [[[1, 0], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1]], // Even row tiles
+                        [[1, 0], [1, 1], [0, 1], [-1, 0], [0, -1], [1, -1]]];  // Odd row tiles
+ 
+// Get the neighbors of the specified tile
+function getNeighbors(tile) {
+    var tilerow = (tile.cord.tiley) % 2; // Even or odd row
+    var neighbors = [];
+ 
+    // Get the neighbor offsets for the specified tile
+    var n = neighborsoffsets[tilerow];
+ 
+    // Get the neighbors
+    for (var i=0; i<n.length; i++) {
+        // Neighbor coordinate
+        var nx = tile.cord.tilex/40 + n[i][0];
+        var ny = tile.cord.tiley/40 + n[i][1];
+ 
+        // Make sure the tile is valid
+        if (nx >= 0 && nx < level.columns && ny >= 0 && ny < level.rows) {
+            var nxx=Math.floor(nx);
+            var nyy=Math.floor(ny);
+            neighbors.push(level.tiles[nyy][nxx]);
+        }
+    }
+ 
+    return neighbors;
+}
