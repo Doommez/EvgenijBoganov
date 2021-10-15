02fe384e626b12d19640a04a1d6ffea245af234e
Doommez <jenyaz862@gmail.com>
2021-10-15 16:22:48 +0300

    create new Rep Clock

---
+++ Clock__class/main.js
@@ -0,0 +1,22 @@
+class Clock{
+    constructor(){
+        this.radiusClock=250;
+        this.radiusHours=20;
+        this.angle=30;
+    }
+    
+}
+class ClockCvs extends Clock{
+    constructor(){
+        super()
+
+    }
+
+}
+
+//model
+
+let clock=new ClockCvs();
+
+//wiu
+
